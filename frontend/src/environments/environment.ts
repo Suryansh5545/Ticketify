5b8d286ef5fc60d37a5d854eb80714f9885cd586
Suryansh5545 <34577232+Suryansh5545@users.noreply.github.com>
2023-07-30 22:06:00 +0530

    Revert all changes to environemtn file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,6 @@
  * Environment URL for local development
  */
 
-const apiEndpoint = process.env["API_ENDPOINT"];
 export const environment = {
     production: false,
     api_endpoint: 'http://localhost:8000/api/',

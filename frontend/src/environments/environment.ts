b5f32f4b402cdde5615497c97c4d80bc0cf05b12
Suryansh5545 <34577232+Suryansh5545@users.noreply.github.com>
2023-07-30 22:02:54 +0530

    Fix env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,11 +3,8 @@
  */
 
 const apiEndpoint = process.env["API_ENDPOINT"];
-const isLocalhost = apiEndpoint === 'http://localhost:8000/api/';
-
 export const environment = {
-  production: !isLocalhost, // Set production to true if it's not localhost
-  api_endpoint: apiEndpoint || 'http://localhost:8000/api/', // Fallback to localhost if API_ENDPOINT is not set
-};
-
+    production: false,
+    api_endpoint: 'http://localhost:8000/api/',
+  };
   

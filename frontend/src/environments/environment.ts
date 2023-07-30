df4323a2f359dc9320e20e99ebea16492f77250d
Suryansh5545 <34577232+Suryansh5545@users.noreply.github.com>
2023-07-30 21:19:57 +0530

    Changed environment variable

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,13 @@
 /**
  * Environment URL for local development
  */
+
+const apiEndpoint = process.env["API_ENDPOINT"];
+const isLocalhost = apiEndpoint === 'http://localhost:8000/api/';
+
 export const environment = {
-    production: false,
-    api_endpoint: 'http://localhost:8000/api/',
-  };
+  production: !isLocalhost, // Set production to true if it's not localhost
+  api_endpoint: apiEndpoint || 'http://localhost:8000/api/', // Fallback to localhost if API_ENDPOINT is not set
+};
+
   

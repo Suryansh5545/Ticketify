bc5d4376009bd963a30f691e23306913e8cc25e3
Suryansh5545 <34577232+Suryansh5545@users.noreply.github.com>
2023-09-23 21:04:05 +0530

    add rules url

--- apps/event/models.py
+++ apps/event/models.py
@@ -21,6 +21,7 @@ class Event(models.Model):
     )
     payment_gateway = models.CharField(max_length=100, choices=pg_options, null=True, blank=True)
     terms_url = models.URLField(max_length=200, blank=True)
+    rules_url = models.URLField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

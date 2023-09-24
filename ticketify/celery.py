341ca267889873ce838941af22440cbd83448ed1
Suryansh Pathak <34577232+Suryansh5545@users.noreply.github.com>
2023-09-25 04:18:38 +0530

    Update celery.py to reduce change time

--- ticketify/celery.py
+++ ticketify/celery.py
@@ -18,7 +18,7 @@ app.config_from_object("django.conf:settings", namespace='CELERY')
 app.conf.beat_schedule = {
     'check-all-transaction-status': {
         'task': 'transactions.tasks.check_all_transaction_status',
-        'schedule': 900.0,  # 15 minutes
+        'schedule': 300.0,  # 5 minutes
     },
     'export-all-data': {
         'task': 'base.tasks.export_all_data',

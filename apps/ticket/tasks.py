e911a56dd25c255ad3821b6318e74c97e7d28357
Suryansh5545 <34577232+Suryansh5545@users.noreply.github.com>
2023-09-26 00:53:46 +0530

    fix days requirement

--- apps/ticket/tasks.py
+++ apps/ticket/tasks.py
@@ -5,5 +5,5 @@ from django.utils import timezone
 
 @shared_task
 def check_old_tickets():
-    Ticket.objects.filter(is_active=False, created_at__lt=timezone.now() - timedelta(minutes=30), ticket_type='REGULAR', transaction_id=None).delete()
+    Ticket.objects.filter(is_active=False, created_at__lt=timezone.now() - timedelta(days=1), ticket_type='REGULAR', transaction_id=None).delete()
     print('Deleted old tickets')
